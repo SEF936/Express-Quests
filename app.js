247ae10cced996f89044c43fd80d797663b6a1cd
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-05-06 17:20:59 +0200

    add validator test inputs values

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const { validateMovie, validateUser } = require("./validator.js");
+
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
@@ -20,11 +22,15 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
 app.get("/api/users/:id", movieHandlers.getUserById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", movieHandlers.postUser);
+// app.post("/api/movies", movieHandlers.postMovie);
+// app.post("/api/users", movieHandlers.postUser);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieHandlers.postUser);
 
-app.put("/api/movies/:id",movieHandlers.updateMovie);
-app.put("/api/users/:id",movieHandlers.updateUser);
+// app.put("/api/movies/:id",movieHandlers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+// app.put("/api/users/:id",movieHandlers.updateUser);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUser);
 
 app.delete("/api/movies/:id",movieHandlers.deleteMovie);
 app.delete("/api/users/:id",movieHandlers.deleteUser);
@@ -36,3 +42,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
