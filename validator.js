04b4bfc27d5ee2efe74c8542b983e28c08e4dfa9
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-05-10 10:15:09 +0200

    add condition to input

--- validator.js
+++ validator.js
@@ -9,15 +9,23 @@ const validateMovie = (req, res , next) => {
     }
     if (director == null) {
         errors.push({ field: "director", message: "This field is required" });
+    }else if ( director.length >= 255) {
+        errors.push({ field: "directo", message: "Should contain less than 255 characters" });
     }
     if (year == null) {
         errors.push({ field: "year", message: "This field is required" });
+    }else if ( year.length >= 255) {
+        errors.push({ field: "year", message: "Should contain less than 255 characters" });
     }
     if (color == null) {
         errors.push({ field: "color", message: "This field is required" });
+    }else if ( color != "1" || color != "0") {
+        errors.push({ field: "color", message: "Should contain 0 or 1" });
     }
     if (duration == null) {
         errors.push({ field: "duration", message: "This field is required" });
+    }else if (isNaN(duration)) {
+        errors.push({ field: "duration", message: "Should contain less than 255 characters" });
     }
     if (errors.length) {
         res.status(422).json({ validationErrors: errors })
@@ -34,20 +42,28 @@ const validateUser= (req, res , next) => {
 
     if (firstname == null) {
         errors.push({ field: "firstname", message: "This field is required" });
-    } 
+    }else if ( firstname.length >= 255) {
+        errors.push({ field: "firstname", message: "Should contain less than 255 characters" });
+    }
     if (lastname == null) {
         errors.push({ field: "lastname", message: "This field is required" });
-    } else if (!emailRegex.test(email)) {
-        errors.push({ field: "email", message: "Invalid email"});
+    }else if ( lastname.length >= 255) {
+        errors.push({ field: "lastname", message: "Should contain less than 255 characters" });
     }
     if (email == null) {
         errors.push({ field: "email", message: "This field is required" });
+    }else if (!emailRegex.test(email)) {
+        errors.push({ field: "email", message: "Invalid email"});
     }
     if (city == null) {
         errors.push({ field: "city", message: "This field is required" });
+    }else if ( city.length >= 255) {
+        errors.push({ field: "city", message: "Should contain less than 255 characters" });
     }
     if (language == null) {
         errors.push({ field: "language", message: "This field is required" });
+    }else if ( language.length >= 255) {
+        errors.push({ field: "language", message: "Should contain less than 255 characters" });
     }
     if (errors.length) {
         res.status(422).json({ validationErrors: errors })
